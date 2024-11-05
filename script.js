137b1593c7c39b59fd0a077e4aeb5ad77ff04dfa
Parikshit Singh <65848309+singh651@users.noreply.github.com>
2024-11-05 05:32:08 +0000

    Just Commiting

---
+++ script.js
@@ -0,0 +1,6 @@
+const Todoinput = document.getElementById('to-do input')
+const addtask = document.getElementById('add-task-btn')
+const todolist = document.getElementById('to-do list')
+
+let tasks = []
+
