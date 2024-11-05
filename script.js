c510de7d108bcc750b7b8a57cbcaa9747f5e1f41
Parikshit Singh <65848309+singh651@users.noreply.github.com>
2024-11-05 05:52:37 +0000

    Change made

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const todolist = document.getElementById('to-do list')
 
 let tasks = []
 
+addtask.addEventListener('click',function(){
+    let tasktext = Todoinput.value.trim();
+    if(tasktext === ""){
+        return;
+    }
+
+    const newtask = {
+        id : Date.now(),
+        text : tasktext,
+        isCompleted : false,
+    };
+    tasks.push(newtask);
+    Todoinput.value = "";
+    console.log(tasks);
+})
